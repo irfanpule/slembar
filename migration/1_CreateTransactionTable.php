1a70a4c301a2bc31fee701743d411f5f8bbac9aa
irfanpule <irfan.biankalang@gmail.com>
2021-11-18 22:58:31 +0700

    modify migration

--- migration/1_CreateTransactionTable.php
+++ migration/1_CreateTransactionTable.php
@@ -11,23 +11,23 @@ class CreateTransactionTable extends \SLiMS\Migration\Migration
 
     function up()
     {
-        \SLiMS\DB::getInstance()->query("CREATE TABLE `payment_transaction` (
+        \SLiMS\DB::getInstance()->query("CREATE TABLE `payment_transactions` (
+            `transaction_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL PRIMARY KEY,
             `member_id` varchar(20) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `transaction_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
             `transaction_time` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `transaction_status` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `order_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
             `payment_type` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `gross_amount` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `fraud_status` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `pdf_url` varchar(255) COLLATE 'utf8mb4_unicode_ci'
+            `pdf_url` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `va_number` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `bank` varchar(255) COLLATE 'utf8mb4_unicode_ci'
-          ) ENGINE='MyISAM' COLLATE 'utf8mb4_unicode_ci';");
+          )");
     }
 
     function down()
     {
-        \SLiMS\DB::getInstance()->query('DROP TABLE `payment_transaction`');
+        \SLiMS\DB::getInstance()->query('DROP TABLE `payment_transactions`');
     }
 }
