ed0e98c5ecb8f319b602897cfa2c10a39bbdd01a
irfanpule <irfan.biankalang@gmail.com>
2021-11-18 16:37:16 +0700

    modification migration transaction table

--- migration/1_CreateTransactionTable.php
+++ migration/1_CreateTransactionTable.php
@@ -14,11 +14,13 @@ class CreateTransactionTable extends \SLiMS\Migration\Migration
         \SLiMS\DB::getInstance()->query("CREATE TABLE `payment_transaction` (
             `member_id` varchar(20) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
             `transaction_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `transaction_time` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `transaction_status` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+            `transaction_time` varchar(255) COLLATE 'utf8mb4_unicode_ci',
+            `transaction_status` varchar(255) COLLATE 'utf8mb4_unicode_ci',
             `order_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `payment_type` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `gross_amount` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL
+            `payment_type` varchar(255) COLLATE 'utf8mb4_unicode_ci',
+            `gross_amount` varchar(255) COLLATE 'utf8mb4_unicode_ci',
+            `fraud_status` varchar(255) COLLATE 'utf8mb4_unicode_ci',
+            `pdf_url` varchar(255) COLLATE 'utf8mb4_unicode_ci'
           ) ENGINE='MyISAM' COLLATE 'utf8mb4_unicode_ci';");
     }
 
