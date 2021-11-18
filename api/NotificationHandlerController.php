b928bce3620a89c2ecdf8690aecd802a69b95770
irfanpule <irfan.biankalang@gmail.com>
2021-11-18 22:58:44 +0700

    add method saveTransaction

--- api/NotificationHandlerController.php
+++ api/NotificationHandlerController.php
@@ -26,4 +26,38 @@ class NotificationHandlerController extends Controller {
         );
         parent::withJson($return);
     }
+
+    public function saveTransaction() {
+        //Receive the RAW post data.
+        $content = trim(file_get_contents("php://input"));
+
+        //Attempt to decode the incoming RAW post data from JSON.
+        $decoded = json_decode($content, true);
+        $va_number = $decoded['midtrans']['va_numbers'][0]['va_number'];
+        $bank = $decoded['midtrans']['va_numbers'][0]['bank'];
+        // sql insert string
+        $sql_str = sprintf("INSERT INTO payment_transactions (
+            member_id, transaction_id, transaction_time,
+            transaction_status, order_id, payment_type,
+            gross_amount, fraud_status, pdf_url, va_number, bank)
+            VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+            $decoded['memberID'], $decoded['midtrans']['transaction_id'], $decoded['midtrans']['transaction_time'],
+            $decoded['midtrans']['transaction_status'], $decoded['midtrans']['order_id'], $decoded['midtrans']['payment_type'],
+            $decoded['midtrans']['gross_amount'], $decoded['midtrans']['fraud_status'], $decoded['midtrans']['pdf_url'],
+            $va_number, $bank);
+
+        $this->db->query($sql_str);
+        if ($this->db->error) {
+            $return = array(
+                "status" => 'error',
+                "message" => $this->db->error
+            );
+        } else {
+            $return = array(
+                "status" => 'success',
+                "message" => 'Berhasil simpan data transaksi'
+            );
+        }
+        parent::withJson($return);
+    }
 }
