e0af60080d5e26d6ddc140fded28f07baf27ec15
irfanpule <irfan.biankalang@gmail.com>
2021-11-20 00:11:56 +0700

    fix migration up didn't work

--- migration/1_CreateTransactionTable.php
+++ migration/1_CreateTransactionTable.php
@@ -11,19 +11,20 @@ class CreateTransactionTable extends \SLiMS\Migration\Migration
 
     function up()
     {
-        \SLiMS\DB::getInstance()->query("CREATE TABLE `payment_transactions` (
-            `transaction_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL PRIMARY KEY,
-            `member_id` varchar(20) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `transaction_time` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `transaction_status` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `order_id` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
-            `payment_type` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `gross_amount` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `fraud_status` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `pdf_url` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `va_number` varchar(255) COLLATE 'utf8mb4_unicode_ci',
-            `bank` varchar(255) COLLATE 'utf8mb4_unicode_ci'
-          )");
+        \SLiMS\DB::getInstance()->query("CREATE TABLE `payment_transactions` ( 
+            `transaction_id` varchar(100) COLLATE 'utf8mb4_unicode_ci' NOT NULL, 
+            `member_id` varchar(20) COLLATE 'utf8mb4_unicode_ci' NOT NULL, 
+            `transaction_time` varchar(100) COLLATE 'utf8mb4_unicode_ci', 
+            `transaction_status` varchar(20) COLLATE 'utf8mb4_unicode_ci', 
+            `order_id` varchar(100) COLLATE 'utf8mb4_unicode_ci' NOT NULL, 
+            `payment_type` varchar(20) COLLATE 'utf8mb4_unicode_ci', 
+            `gross_amount` varchar(150) COLLATE 'utf8mb4_unicode_ci', 
+            `fraud_status` varchar(20) COLLATE 'utf8mb4_unicode_ci', 
+            `pdf_url` varchar(220) COLLATE 'utf8mb4_unicode_ci', 
+            `va_number` varchar(30) COLLATE 'utf8mb4_unicode_ci', 
+            `bank` varchar(50) COLLATE 'utf8mb4_unicode_ci', 
+            PRIMARY KEY (`transaction_id`)) 
+            ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
     }
 
     function down()
